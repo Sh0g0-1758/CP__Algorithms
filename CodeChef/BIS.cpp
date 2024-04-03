f6c89d9a02be54542e0019d146ca4ea9587fdee1
Sh0g0-1758 <shouryagoel10000@gmail.com>
2024-04-04 00:22:29 +0530

    CodeChef Contest

---
+++ CodeChef/BIS.cpp
@@ -0,0 +1,72 @@
+// All complex things are inherently simple. If you can't explain something simply,
+// you just don't understand it well enough.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+
+#define endl "\n"
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define MOD 1000000007
+#define INF 1e18
+#define setbits(x) __builtin_popcountll(x)
+using cd = complex<double>;
+const double PI=acos(-1);
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+ 
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(lld t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(ull t) {cerr << t;}
+ 
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+
+int main()
+{
+    fastio();
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        ll n;
+        cin >> n;
+        string s;
+        cin >> s;
+        ll n11=0,n10=0,n01=0,n00=0;
+        for(ll i = 0; i < n;i+=2) {
+            pair<ll,ll> p;
+            p.first=s.at(i)-'0';
+            p.second=s.at(i+1)-'0';
+            if(p.first==0 and p.second==0) n00++;
+            else if(p.first==0 and p.second==1) n01++;
+            else if(p.first==1 and p.second==0) n10++;
+            else if(p.first==1 and p.second==1) n11++;
+        }
+        ll ans = 2 * n00 + 2 * n11;
+        if(n10>0) ans++,n10--;
+        if(n10>0) ans++,n10--;
+        if(n01>0) ans+=2,n01--;
+        cout << ans << endl;
+    }
+    return 0;
+}
---
+++ CodeChef/MOOCHEF.cpp
@@ -0,0 +1,71 @@
+// All complex things are inherently simple. If you can't explain something simply,
+// you just don't understand it well enough.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+
+#define endl "\n"
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define MOD 1000000007
+#define INF 1e18
+#define setbits(x) __builtin_popcountll(x)
+using cd = complex<double>;
+const double PI=acos(-1);
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+ 
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(lld t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(ull t) {cerr << t;}
+ 
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+
+int main()
+{
+    fastio();
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        ll n,l,r;
+        cin >> n >> l >> r;
+        ll max_possible = 0;
+        ll min_possible = 0;
+        ll h = 0;
+        for(ll i=0;i<n;i++)
+        {
+            ll elem;
+            cin >> elem;
+            if(elem >= l and elem <= r) {
+                h++;
+            } else {
+                h--;
+            }
+            max_possible = max(max_possible,h);
+            min_possible = min(min_possible,h);
+        }
+        cout << max_possible << " " << min_possible << endl;
+    }
+    return 0;
+}
