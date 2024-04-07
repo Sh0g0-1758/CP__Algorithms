0e710818b8094327956e4920309d22a17ff1d4bb
Sh0g0-1758 <shouryagoel10000@gmail.com>
2024-04-07 15:34:45 +0530

    Learned some new things

--- CodeChef/PRTWHY.cpp
+++ CodeChef/PRTWHY.cpp
@@ -41,13 +41,65 @@ template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i
 template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
 template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
 
+map<pair<ll,ll> ,ll> mult;
+
+void dfs(ll v, ll p, vector<bool> &visited, vector<ll> &tin, vector<ll> &low, vector<vector<ll>> & adj, ll &timer,ll &ans) {
+    visited[v] = true;
+    tin[v] = low[v] = timer++;
+    for (auto w : adj[v]) {
+        if (w == p) continue;
+        if (visited[w]) {
+            low[v] = min(low[v], tin[w]);
+        } else {
+            dfs(w, v, visited, tin, low, adj, timer,ans);
+            low[v] = min(low[v], low[w]);
+            if (low[w] > tin[v]) {
+                if(mult[{v,w}]==1){
+                    ans++;
+                }
+            }
+        }
+    }
+}
+
+void find_bridges(ll n, vector<vector<ll>> &adj, ll& ans) {
+    vector<pair<ll,ll>> bridges;
+    ll timer = 0;
+    vector<bool> visited(n,false);
+    vector<ll> tin(n,-1), low(n,-1);
+    for (int i = 0; i < n; ++i) {
+        if (!visited[i])
+            dfs(i, -1, visited, tin, low, adj, timer,ans);
+    }
+}
+
 int main()
 {
     fastio();
-    ll t;
-    cin >> t;
+    ll t = 1;
+    // cin >> t;
     while (t--)
     {
+        ll n,m;
+        cin>>n>>m;
+        vector<vector<ll>> graph(n);
+
+        for(ll i = 0; i < m;i++) {
+            ll a,b;
+            cin>>a>>b;
+            if (mult.find({a-1,b-1})!=mult.end()) {
+                mult[{a-1,b-1}]++;
+                mult[{b-1,a-1}]++;
+                continue;
+            }
+            graph[a-1].push_back(b-1);
+            graph[b-1].push_back(a-1);
+            mult[{a-1,b-1}]=1;
+            mult[{b-1,a-1}]=1;
+        }
+        ll cnt = 0;
+        find_bridges(n, graph,cnt);
+        cout << m-cnt << endl;
     }
     return 0;
 }
