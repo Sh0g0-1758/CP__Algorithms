906e9e11633157705b5ba8fb3aedbe6f25f25f5f
Sh0g0-1758 <shouryagoel10000@gmail.com>
2024-05-25 18:35:11 +0530

    Gaining confidence in DP

---
+++ cses/Rectangle_Cutting.cpp
@@ -0,0 +1,75 @@
+// All complex things are inherently simple. If you can't explain something simply,
+// you just don't understand it well enough.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+
+#define endl "\n"
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define MOD 1000000007
+#define INF 1e18
+#define setbits(x) __builtin_popcountll(x)
+using cd = complex<double>;
+const double PI=acos(-1);
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(lld t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(ull t) {cerr << t;}
+
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.first); cerr << ","; _print(p.second); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(unordered_map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(stack <T> v) {cerr << "[ "; while (!v.empty()) {_print(v.top()); cerr << " "; v.pop();} cerr << "]";}
+template <class T> void _print(queue <T> v) {cerr << "[ "; while (!v.empty()) {_print(v.front()); cerr << " "; v.pop();} cerr << "]";}
+template <class T> void _print(priority_queue <T> v) {cerr << "[ "; while (!v.empty()) {_print(v.top()); cerr << " "; v.pop();} cerr << "]";}
+template <class T> void _print(deque <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+
+int main()
+{
+    fastio();
+    ll t = 1;
+    // cin >> t;
+    while (t--)
+    {
+        ll n,m;cin>>n>>m;
+        vector<vector<ll>> dp(n+1,vector<ll>(m+1,INT_MAX));
+        for(ll i = 1;i<=n;i++) {
+            for(ll j = 1;j<=m;j++) {
+                if(i == j) dp[i][j] = 0;
+                else {
+                for (ll k = 1; k < j; k++) {
+                    dp[i][j]
+                        = min(dp[i][j],
+                              dp[i][k] + dp[i][j - k] + 1);
+                }
+                for (ll k = 1; k < i; k++) {
+                    dp[i][j]
+                        = min(dp[i][j],
+                              dp[k][j] + dp[i - k][j] + 1);
+                }
+
+                }
+            }
+        }
+        // debug(dp)
+        cout<<dp[n][m]<<endl;
+    }
+    return 0;
+}
