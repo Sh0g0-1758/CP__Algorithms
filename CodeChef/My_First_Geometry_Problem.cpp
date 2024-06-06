ee97d88d16e36e0edfddae2ea0c415e02d622e5a
Sh0g0-1758 <shouryagoel10000@gmail.com>
2024-06-06 13:15:34 +0530

    Codechef Contests are shit

---
+++ CodeChef/My_First_Geometry_Problem.cpp
@@ -0,0 +1,73 @@
+// All complex things are inherently simple. If you can't explain something simply,
+// you just don't understand it well enough.
+
+
+// ##################################
+// ##  author: __Legacy__          ##
+// ##  date: 05-06-2024 20:05:47   ##
+// ##################################
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+
+
+#define endl "\n"
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define MOD 1000000007
+#define INF 1e18
+#define setbits(x) __builtin_popcountll(x)
+using cd = complex<double>;
+const double PI=acos(-1);
+
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+
+
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(lld t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(ull t) {cerr << t;}
+
+
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.first); cerr << ","; _print(p.second); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(unordered_map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(stack <T> v) {cerr << "[ "; while (!v.empty()) {_print(v.top()); cerr << " "; v.pop();} cerr << "]";}
+template <class T> void _print(queue <T> v) {cerr << "[ "; while (!v.empty()) {_print(v.front()); cerr << " "; v.pop();} cerr << "]";}
+template <class T> void _print(priority_queue <T> v) {cerr << "[ "; while (!v.empty()) {_print(v.top()); cerr << " "; v.pop();} cerr << "]";}
+template <class T> void _print(deque <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+
+
+int main()
+{
+    fastio();
+    ll t = 1;
+    cin >> t;
+    while (t--)
+    {
+        string s;cin>>s;
+        if(s == "1000" or s == "0100" or s == "0010" or s == "0001") cout << 11 << endl;
+        else if (s == "1100" or s == "0011") cout << 21 << endl;
+        else if (s == "1010" or s == "1001" or s == "0110" or s == "0101") cout << 121 << endl;
+        else if (s == "1111") cout << 441 << endl;
+        else if (s == "1110" or s == "1101" or s == "1011" or s == "0111") cout << 231 << endl;
+        else if (s == "0000") cout << 1 << endl;
+    }
+    return 0;
+}
