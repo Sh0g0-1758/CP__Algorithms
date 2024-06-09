2d67561a9bcf578a4ef064ad4e9e6e6047a45ec2
Sh0g0-1758 <shouryagoel10000@gmail.com>
2024-06-10 01:32:16 +0530

    Div 2 today. Got a positive delta

---
+++ CodeForces/B_Large_Addition.cpp
@@ -0,0 +1,86 @@
+// All complex things are inherently simple. If you can't explain something simply,
+// you just don't understand it well enough.
+
+
+// ##################################
+// ##  author: __Legacy__          ##
+// ##  date: 09-06-2024 20:17:14   ##
+// ##################################
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+
+
+#define endl "\n"
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define MOD 1000000007
+#define invec(a) for(auto &i:a)cin>>i;
+#define INF 1e18
+#define setbits(x) __builtin_popcountll(x)
+using cd = complex<double>;
+const double PI=acos(-1);
+
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+
+
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(lld t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(ull t) {cerr << t;}
+
+
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.first); cerr << ","; _print(p.second); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(unordered_map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(stack <T> v) {cerr << "[ "; while (!v.empty()) {_print(v.top()); cerr << " "; v.pop();} cerr << "]";}
+template <class T> void _print(queue <T> v) {cerr << "[ "; while (!v.empty()) {_print(v.front()); cerr << " "; v.pop();} cerr << "]";}
+template <class T> void _print(priority_queue <T> v) {cerr << "[ "; while (!v.empty()) {_print(v.top()); cerr << " "; v.pop();} cerr << "]";}
+template <class T> void _print(deque <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+
+
+int main()
+{
+    fastio();
+    ll t = 1;
+    cin >> t;
+    while (t--)
+    {
+        ll n;cin>>n;
+        ll f = 0;
+        string s = to_string(n);
+        for(ll i = 0; i < s.size() - 2;i++) {
+            ll x = n%10;
+            if(x == 9) {
+                f += 1;
+                break;
+            }
+            n = n/10 - 1;
+        }
+        if(n > 18 or n < 10) {
+            f += 1;
+        }
+        if(f) {
+            cout<<"NO"<<endl;
+        } else {
+            cout<<"YES"<<endl;
+        }
+    }
+    return 0;
+}
---
+++ CodeForces/C_1_Magnitude_Easy_Version.cpp
@@ -0,0 +1,85 @@
+// All complex things are inherently simple. If you can't explain something simply,
+// you just don't understand it well enough.
+
+
+// ##################################
+// ##  author: __Legacy__          ##
+// ##  date: 09-06-2024 20:50:24   ##
+// ##################################
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+
+
+#define endl "\n"
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define MOD 1000000007
+#define invec(a) for(auto &i:a)cin>>i;
+#define INF 1e18
+#define setbits(x) __builtin_popcountll(x)
+using cd = complex<double>;
+const double PI=acos(-1);
+
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+
+
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(lld t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(ull t) {cerr << t;}
+
+
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.first); cerr << ","; _print(p.second); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(unordered_map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(stack <T> v) {cerr << "[ "; while (!v.empty()) {_print(v.top()); cerr << " "; v.pop();} cerr << "]";}
+template <class T> void _print(queue <T> v) {cerr << "[ "; while (!v.empty()) {_print(v.front()); cerr << " "; v.pop();} cerr << "]";}
+template <class T> void _print(priority_queue <T> v) {cerr << "[ "; while (!v.empty()) {_print(v.top()); cerr << " "; v.pop();} cerr << "]";}
+template <class T> void _print(deque <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+
+
+int main()
+{
+    fastio();
+    ll t = 1;
+    cin >> t;
+    while (t--)
+    {
+        ll n;cin>>n;
+        vector<ll> a(n);invec(a)
+        ll t_sum = 0;
+        for(auto it : a) {
+            t_sum += it;
+        }
+        if(t_sum < 0) {
+            cout << abs(t_sum) << endl;
+        } else {
+            t_sum = 0;
+            for(auto it : a) {
+                t_sum += it;
+                if(t_sum < 0) {
+                    t_sum = abs(t_sum);
+                }
+            }
+            cout<<t_sum<<endl;
+        }
+    }
+    return 0;
+}
