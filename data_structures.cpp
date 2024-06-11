88f945e5e0ddd9023e1e04e6dcb1206eaf22919b
Sh0g0-1758 <shouryagoel10000@gmail.com>
2024-06-11 13:22:35 +0530

    Solved 10 leetcode

--- data_structures.cpp
+++ data_structures.cpp
@@ -45,3 +45,14 @@ public:
         }
     }
 };
+
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
