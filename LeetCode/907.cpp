a8aa1566b639ecaa8d23052960b4ecacf82269c8
Sh0g0-1758 <shouryagoel10000@gmail.com>
2024-07-05 13:00:45 +0530

    Solved a problem

--- LeetCode/907.cpp
+++ LeetCode/907.cpp
@@ -1,33 +1,31 @@
 class Solution {
 public:
-  int sumSubarrayMins(vector<int>& A) {
-    stack<pair<int, int>> in_stk_p, in_stk_n;
-    vector<int> left(A.size()), right(A.size());
+    int mod = 1e9 + 7;
+    int sumSubarrayMins(vector<int>& nums) {
+        int n = nums.size();
+        stack<int> stck;
+        vector<int> minPrev(n,-1), minNext(n,n);
 
-    for(int i = 0; i < A.size(); i++) left[i] =  i + 1;
-    for(int i = 0; i < A.size(); i++) right[i] = A.size() - i;
+        for(int i = 0; i < n;i++) {
+            while(!stck.empty() and nums[stck.top()] >= nums[i]) stck.pop();
+            if(!stck.empty()) minPrev[i] = stck.top();
+            stck.push(i);
+        }
 
-    for(int i = 0; i < A.size(); i++) {
+        while(!stck.empty()) stck.pop();
 
-      while(!in_stk_p.empty() && in_stk_p.top().first > A[i]) in_stk_p.pop();
-      left[i] = in_stk_p.empty() ? i + 1 : i - in_stk_p.top().second;
-      in_stk_p.push({A[i],i});
+        for(int i = n -1; i >= 0;i--) {
+            while(!stck.empty() and nums[stck.top()] > nums[i]) stck.pop();
+            if(!stck.empty()) minNext[i] = stck.top();
+            stck.push(i);
+        }
 
-      while(!in_stk_n.empty() && in_stk_n.top().first > A[i]){
-        auto x = in_stk_n.top();in_stk_n.pop();
-        right[x.second] = i - x.second;
-      }
-
-      in_stk_n.push({A[i], i});
-
+        long long ans = 0;
+        for(int i = 0; i < n;i++) {
+            long long minPrevDistance = i - minPrev[i];
+            long long minNextDistance = minNext[i] - i;
+            ans = (ans + (long long)(minPrevDistance) * (long long)(minNextDistance) * (long long)nums[i])%mod;
+        }
+        return ans;
     }
-
-    long long ans = 0, mod = 1e9 +7;
-
-    for(int i = 0; i < A.size(); i++){
-      ans = (ans + (long long)A[i]*(long long)left[i]*(long long)right[i])%mod;
-    }
-
-    return ans;
-  }
 };
